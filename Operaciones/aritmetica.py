ada632f2c47a623512e7f403695f0b145cd79056
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-14 17:10:55 -0600

    operaciones aritmeticas
    suma, resta, division , multiplicacion

--- Operaciones/aritmetica.py
+++ Operaciones/aritmetica.py
@@ -1,2 +1,19 @@
-def suma():
-    print('sumando')
+def suma(num1,num2):
+    sumar = num1 + num2
+    print('• Sumando:   ',str(num1),' + ',str(num2), ' = ', sumar)
+    return sumar
+
+def resta(num1,num2):
+    restar = num1 - num2
+    print('• Restando:  ',str(num1),' - ',str(num2), ' = ', restar)
+    return restar
+
+def multiplicacion(num1,num2):
+    multiplicar = num1 * num2
+    print('• Multiplicando:  ',str(num1),' * ',str(num2), ' = ', multiplicar)
+    return multiplicar
+
+def division(num1,num2):
+    dividir = num1 / num2
+    print('• Dividiendo:  ',str(num1),' / ',str(num2), ' = ', dividir)
+    return dividir
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from Operaciones import *
 
-aritmetica.suma()
+aritmetica.suma(5,6)
+aritmetica.resta(3,1)
+aritmetica.multiplicacion(5,5)
+aritmetica.division(15,5)
