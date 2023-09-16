08a8c85548deaa866830192a92fe0f3fd1f4fa01
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-16 14:15:16 -0600

    tkinter crear botones
    solo interfaz grafica sin funciones

--- main.py
+++ main.py
@@ -31,8 +31,18 @@ def hola():
 raiz = Tk()
 raiz.title('LFP Proyecto 1 | 201906795')
 raiz.geometry('900x500')
-lbl = Label(raiz,text='label1')
-lbl.pack()
-btn = Button(raiz, text='boton1', command= hola)
-btn.pack()
+# lbl = Label(raiz,text='label1')
+# lbl.pack()
+# btn = Button(raiz, text='boton1', command= hola)
+# btn.pack()
+
+btnanalizar = Button(raiz,text='Analizar', bg='#313446', fg='#FFFFFF')
+btnanalizar.place(x=200,y=10, width=80, height= 35)
+
+btnerrores = Button(raiz,text='Errores', bg='#313446', fg='#FFFFFF')
+btnerrores.place(x=340,y=10, width=80, height= 35)
+
+btnreportes = Button(raiz,text='Reportes', bg='#313446', fg='#FFFFFF')
+btnreportes.place(x=480,y=10, width=80, height= 35)
+
 raiz.mainloop()
