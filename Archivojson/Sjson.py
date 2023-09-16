cb09830d1fd4305e1c2f795e383548e3e5bfa8d8
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-16 13:09:26 -0600

    funcion leer archivo

--- Archivojson/Sjson.py
+++ Archivojson/Sjson.py
@@ -1,2 +1,16 @@
+import os
+
 def hola():
     print("hola mundo")
+
+
+def abrirarchivo():
+    ruta = 'test.json'
+    nombre, extension = os.path.splitext(ruta)
+    print(nombre, extension)
+
+    try:
+        with open(ruta, "r") as archivo:
+            print(archivo.read())
+    except:
+        print("♦ Error: no se pudo abrir el archivo.")
