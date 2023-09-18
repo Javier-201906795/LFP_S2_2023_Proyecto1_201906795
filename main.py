3b7013f901d19a60591cddca67e3a1151557d90b
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-18 09:21:30 -0600

    tkinter funciones
    se crearon las funciones para cada boton

--- main.py
+++ main.py
@@ -4,6 +4,37 @@ from tkinter.ttk import Combobox
 from Operaciones import *
 from Archivojson import *
 
+
+#######################################################################
+def Abrir():
+    print('abrir')
+
+def Guardar():
+    print('Guardar')
+
+def Guardarcomo():
+    print('Guardarcomo')
+
+def Salir():
+    print('Salir')
+
+
+
+
+def Analizar():
+    print('analizar')
+
+def Errores():
+    print('errores')
+
+def Reporte():
+    print('reporte')
+
+
+
+
+
+########################################################################
 aritmetica.suma(5,6)
 aritmetica.resta(3,1)
 aritmetica.multiplicacion(5,5)
@@ -34,19 +65,36 @@ raiz = Tk()
 raiz.title('LFP Proyecto 1 | 201906795')
 raiz.geometry('900x500')
 
-btnanalizar = Button(raiz,text='Analizar', bg='#313446', fg='#FFFFFF', command=listaseleccion)
+btnanalizar = Button(raiz,text='Analizar', bg='#313446', fg='#FFFFFF', command=Analizar)
 btnanalizar.place(x=230,y=10, width=80, height= 35)
 
-btnerrores = Button(raiz,text='Errores', bg='#313446', fg='#FFFFFF')
+btnerrores = Button(raiz,text='Errores', bg='#313446', fg='#FFFFFF', command=Errores)
 btnerrores.place(x=370,y=10, width=80, height= 35)
 
-btnreportes = Button(raiz,text='Reportes', bg='#313446', fg='#FFFFFF')
+btnreportes = Button(raiz,text='Reportes', bg='#313446', fg='#FFFFFF', command=Reporte)
 btnreportes.place(x=510,y=10, width=80, height= 35)
 
 opcionesarchivo = ['ARCHIVO','Abrir','Guardar','Guardar como', 'Salir']
 listaopcionesarchivo = Combobox(raiz, text='Archivo', values = opcionesarchivo)
 listaopcionesarchivo.place(x=50,y=12, width=130, height= 30)
 listaopcionesarchivo.current(0)
+
+#Switch case
+def seleccion(event):
+    print('Seleccionando ->', listaopcionesarchivo.get())
+    sel = listaopcionesarchivo.get()
+    if sel == 'Abrir':
+        Abrir()
+    elif sel == 'Guardar':
+        Guardar()
+    elif sel == 'Guardar como':
+        Guardarcomo()
+    elif sel == 'Salir':
+        Salir()
+
+#Ejecuta la opcion cuando cambia de seleccion
+listaopcionesarchivo.bind('<<ComboboxSelected>>',seleccion)
+
 
 inputtexto = Text(raiz,padx=40)
 inputtexto.place(x=80,y=100, width=800, height=350)
