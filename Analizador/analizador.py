7bf495f83baae1a8193e4477b70482ffd10404f3
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-20 16:39:19 -0600

    comentar codigo
    no se va utlizar diccionarios para leer el documento

--- Analizador/analizador.py
+++ Analizador/analizador.py
@@ -7,26 +7,31 @@ from Archivojson import *
 
 
 
-def convertiradiccionario(texto):
-    try:
-        newdiccionario = Sjson.diccionarioJSON(texto)
-        #Evaluar si es un error
-        mensaje = str(newdiccionario)[2:7]
-        if mensaje == 'Error':
-            #causar un error
-            x = 1/0
-        else:
-            #Retornar
-            return newdiccionario
-    except Exception:
-        print('Error:[analizado.py][CG002] ', Exception)
-        MessageBox.showerror('Error:[analizado.py][CG002]','No se pudo convertir un diccionario revise el texto introducido')
+# def convertiradiccionario(texto):
+#     try:
+#         newdiccionario = Sjson.diccionarioJSON(texto)
+#         #Evaluar si es un error
+#         mensaje = str(newdiccionario)[2:7]
+#         if mensaje == 'Error':
+#             #causar un error
+#             x = 1/0
+#         else:
+#             #Retornar
+#             return newdiccionario
+#     except Exception:
+#         print('Error:[analizado.py][CG002] ', Exception)
+#         MessageBox.showerror('Error:[analizado.py][CG002]','No se pudo convertir un diccionario revise el texto introducido')
 
 
 def lexico(texto):
-    #Convertir texto a diccionario
-    diccionario = convertiradiccionario(texto)
-    print(diccionario)
+
+
+    # #Convertir texto a diccionario
+    # diccionario = convertiradiccionario(texto)
+    # #Obtener operaciones
+    # listaOperaciones = diccionario['operaciones']
+    # print(listaOperaciones)
     
-    
+    pass
+
     
