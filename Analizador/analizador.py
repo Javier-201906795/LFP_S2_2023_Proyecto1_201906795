143ef9d56d4d0b08ad215daf56855addfd54ec3b
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-21 09:16:35 -0600

    eliminar comentarios

--- Analizador/analizador.py
+++ Analizador/analizador.py
@@ -7,30 +7,13 @@ from Archivojson import *
 
 
 
-# def convertiradiccionario(texto):
-#     try:
-#         newdiccionario = Sjson.diccionarioJSON(texto)
-#         #Evaluar si es un error
-#         mensaje = str(newdiccionario)[2:7]
-#         if mensaje == 'Error':
-#             #causar un error
-#             x = 1/0
-#         else:
-#             #Retornar
-#             return newdiccionario
-#     except Exception:
-#         print('Error:[analizado.py][CG002] ', Exception)
 #         MessageBox.showerror('Error:[analizado.py][CG002]','No se pudo convertir un diccionario revise el texto introducido')
 
 
 def lexico(texto):
 
 
-    # #Convertir texto a diccionario
-    # diccionario = convertiradiccionario(texto)
-    # #Obtener operaciones
-    # listaOperaciones = diccionario['operaciones']
-    # print(listaOperaciones)
+   
     
     #Leer Linea por linea
     linea = 1
