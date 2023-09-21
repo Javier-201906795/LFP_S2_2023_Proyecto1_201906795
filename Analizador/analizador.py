af30db251706b9e22ee2e16b61f1d4a86141a374
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-21 13:54:10 -0600

    analizador lexico
    obtener posibles tokens

--- Analizador/analizador.py
+++ Analizador/analizador.py
@@ -16,7 +16,23 @@ tokens = []
 linea = 1
 columna = 1
 
+################################################################
 
+
+
+
+def obtenertexto(text, c):
+    #Texto
+    string = ''
+    #Evaluar caracter por carcater
+    for caracter in text:
+        if caracter == '"':
+            #Si encuntra el cierre "
+            return [string, c]
+        #Forma el texto
+        string += caracter
+        c += 1
+    print("Error: No se encontraron comillas doble que cerraran el texto.")
 
 
 
@@ -35,8 +51,30 @@ def obtenertokens(texto):
         #Obtener caracter
         caracter = texto[c]
         
-
-
+        #Evaluar
+        if caracter.isspace():
+            #Si es algun tipo de espacio
+            if caracter == '\n':
+                #Si es un Salto de linea
+                #Aumenta la linea
+                linea += 1
+                #Reinicia columnas
+                columna = 1
+            elif caracter == '\t':
+                #Si es un Tabulador
+                #Aumenta la columna en 4
+                columna += 4
+            else:
+                #Si es un espacio
+                #Aumenta la columna
+                columna += 1
+            #Reporte
+            #print('Caracter: ', caracter, ' Linea: ', linea, ' Columna: ',columna)
+        elif caracter == '"':
+            #Si es un texto un posible token
+            textoaevaluar = texto[c+1:]
+            string, pos = obtenertexto(textoaevaluar, c)
+            print('token: ', string, ' pos: ', pos)
         c += 1
     #Resultados
     print('Iteraciones maxima: ', maxiterc)
