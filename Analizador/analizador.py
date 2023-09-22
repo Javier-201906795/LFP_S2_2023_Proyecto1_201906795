f6a067a716f6b4279ef86c463d5583d2f0ed90e4
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-22 17:21:26 -0600

    crear estructura funciones graphviz

--- Analizador/analizador.py
+++ Analizador/analizador.py
@@ -388,17 +388,22 @@ def graphviz():
     #system('dot -Tpng archivo.dot -o archivograph1.png')
 
 
-    dot = Digraph('Grafica',filename='Grafica1', format='png')
-    dot.attr(rankdir='LR', size='8,5')
-    dot.attr('node', shape='circle')
-    dot.node('x1','10')
-    dot.node('x2','25')
-    dot.node('x3','suma\n35')
-    dot.edge('x3','x1')
-    dot.edge('x3','x2')
-    dot.render('Graficapng', view=False)
+    # dot = Digraph('Grafica',filename='Grafica1', format='png')
+    # dot.attr(rankdir='LR', size='8,5')
+    # dot.attr('node', shape='circle')
+    # dot.node('x1','10')
+    # dot.node('x2','25')
+    # dot.node('x3','suma\n35')
+    # dot.edge('x3','x1')
+    # dot.edge('x3','x2')
+    # dot.render('Graficapng', view=False)
 
     Arbol.hola()
+    Arbol.generagraficaarbol()
+    Arbol.configuraciones()
+    Arbol.agregarnodo()
+    Arbol.conectarnodo()
+    Arbol.render()
 
 
     
--- Grafica/Arbol.py
+++ Grafica/Arbol.py
@@ -1,4 +1,33 @@
+from graphviz import *
+from graphviz import Digraph
 
+
+dot = None
 
 def hola():
     print('hola')
+
+
+def generagraficaarbol():
+    global dot
+    dot = Digraph('Grafica',filename='Grafica1', format='png')
+    dot.attr(rankdir='LR', size='8,5')
+
+def configuraciones():
+    global dot
+    dot.attr('node', shape='circle')
+
+def agregarnodo():
+    global dot
+    dot.node('x1','10')
+    dot.node('x2','25')
+    dot.node('x3','suma\n35')
+
+def conectarnodo():
+    global dot
+    dot.edge('x3','x1')
+    dot.edge('x3','x2')
+
+def render():
+    global dot
+    dot.render('Graficapng', view=True)
