1d90421ed86abd32161c8bc2e333a69b56fc869f
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-09-22 12:30:38 -0600

    se eliminaron comentarios

--- Analizador/analizador.py
+++ Analizador/analizador.py
@@ -263,16 +263,6 @@ def interpretar_instruccion(operacion, valor1, valor2):
 
 ################################################################    
 def evaluar_tipo_operacion(operacion, valor1, valor2):
-    # aritmetica.suma(5,6)
-    # aritmetica.resta(3,1)
-    # aritmetica.multiplicacion(5,5)
-    # aritmetica.division(15,5)
-    # aritmetica.potencia(5,2)
-    # aritmetica.raiz(25,2)
-    # aritmetica.inverso(5,3)
-    # aritmetica.seno(45)
-    # aritmetica.coseno(45)
-    # aritmetica.tangente(45)
 
     #Convertir texto a minusculas
     operacion = operacion.lower()
